acbdd8375a774867d9b1ec3db244fc62445a8e04
Pedro Pablo Figueroa Miranda <pedro.figueroa@silaba.org>
2018-03-27 10:13:52 -0500

    Initial Commit

---
+++ Silaba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silaba
+//
+//  Created by Pedro Pablo Figueroa Miranda on 27/03/18.
+//  Copyright © 2018 Silaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
