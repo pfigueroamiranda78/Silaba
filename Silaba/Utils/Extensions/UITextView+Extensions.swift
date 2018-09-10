15da0c60a2d8f73fde7c721b098b2815efec2480
Pedro Pablo Figueroa Miranda <pfigueroamiranda@gmail.com>
2018-09-10 14:18:37 -0500

    V1 Remote

---
+++ Silaba/Utils/Extensions/UITextView+Extensions.swift
@@ -0,0 +1,166 @@
+//
+//  UITextField+Extensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 15/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+/// Extend UITextView and implemented UITextViewDelegate to listen for changes
+extension UITextView: UITextViewDelegate {
+    
+    
+    /// Resize the placeholder when the UITextView bounds change
+    override open var bounds: CGRect {
+        didSet {
+            self.resizePlaceholder()
+        }
+    }
+    
+    /// The UITextView placeholder text
+    public var placeholder: String? {
+        get {
+            var placeholderText: String?
+            
+            if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+                placeholderText = placeholderLabel.text
+            }
+            
+            return placeholderText
+        }
+        set {
+            if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+                placeholderLabel.text = newValue
+                placeholderLabel.sizeToFit()
+            } else {
+                self.addPlaceholder(newValue!)
+            }
+        }
+    }
+    
+    /// When the UITextView did change, show or hide the label based on if the UITextView is empty or not
+    ///
+    /// - Parameter textView: The UITextView that got updated
+    public func textViewDidChange(_ textView: UITextView) {
+        if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+            placeholderLabel.isHidden = self.text.count > 0
+        }
+       
+    }
+    
+    /// Resize the placeholder UILabel to make sure it's in the same position as the UITextView text
+    private func resizePlaceholder() {
+        if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+            let labelX = self.textContainer.lineFragmentPadding
+            let labelY = self.textContainerInset.top - 2
+            let labelWidth = self.frame.width - (labelX * 2)
+            let labelHeight = placeholderLabel.frame.height
+            
+            placeholderLabel.frame = CGRect(x: labelX, y: labelY, width: labelWidth, height: labelHeight)
+        }
+    }
+    
+    /// Adds a placeholder UILabel to this UITextView
+    private func addPlaceholder(_ placeholderText: String) {
+        let placeholderLabel = UILabel()
+        
+        placeholderLabel.text = placeholderText
+        placeholderLabel.sizeToFit()
+        
+        placeholderLabel.font = self.font
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.tag = 100
+        
+        placeholderLabel.isHidden = self.text.count > 0
+        
+        self.addSubview(placeholderLabel)
+        self.resizePlaceholder()
+        self.delegate = self
+    }
+    
+    
+    
+}
+
+
+
+func += <KeyType, ValueType> ( left: inout Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+    for (k, v) in right {
+        left.updateValue(v, forKey: k)
+    }
+}
+    
+
+extension UITextView {
+   
+    public func resolveHashTags(possibleUserDisplayNames:[String]? = nil) {
+        
+        let schemeMap = [
+            "#":"hash",
+            "@":"mention"
+        ]
+        
+        // Separate the string into individual words.
+        // Whitespace is used as the word boundary.
+        // You might see word boundaries at special characters, like before a period.
+        // But we need to be careful to retain the # or @ characters.
+        let words = self.text.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
+        let attributedString = attributedText.mutableCopy() as! NSMutableAttributedString
+        
+        // keep track of where we are as we interate through the string.
+        // otherwise, a string like "#test #test" will only highlight the first one.
+        var bookmark = text.startIndex
+        
+        // Iterate over each word.
+        // So far each word will look like:
+        // - I
+        // - visited
+        // - #123abc.go!
+        // The last word is a hashtag of #123abc
+        // Use the following hashtag rules:
+        // - Include the hashtag # in the URL
+        // - Only include alphanumeric characters.  Special chars and anything after are chopped off.
+        // - Hashtags can start with numbers.  But the whole thing can't be a number (#123abc is ok, #123 is not)
+        for word in words {
+            
+            var scheme:String? = nil
+            
+            if word.hasPrefix("#") {
+                scheme = schemeMap["#"]
+            } else if word.hasPrefix("@") {
+                scheme = schemeMap["@"]
+            }
+            
+            // Drop the # or @
+            var wordWithTagRemoved = String(word.dropFirst())
+            
+            // Drop any trailing punctuation
+            wordWithTagRemoved.dropTrailingNonAlphaNumericCharacters()
+            
+            // Make sure we still have a valid word (i.e. not just '#' or '@' by itself, not #100)
+            guard let schemeMatch = scheme, Int(wordWithTagRemoved) == nil && !wordWithTagRemoved.isEmpty
+                else { continue }
+            
+            let remainingRange = Range(bookmark..<text.endIndex)
+            
+            // URL syntax is http://123abc
+            
+            // Replace custom scheme with something like hash://123abc
+            // URLs actually don't need the forward slashes, so it becomes hash:123abc
+            // Custom scheme for @mentions looks like mention:123abc
+            // As with any URL, the string will have a blue color and is clickable
+            
+            if let matchRange = text.range(of: word, options: .literal, range: remainingRange),
+                let escapedString = wordWithTagRemoved.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                attributedString.addAttribute(NSAttributedStringKey.link, value: "\(schemeMatch):\(escapedString)", range: text.NSRangeFromRange(range: matchRange)!)
+            }
+            
+            // just cycled through a word. Move the bookmark forward by the length of the word plus a space
+            bookmark = text.index(bookmark, offsetBy: word.count)
+        }
+        
+        self.attributedText = attributedString
+    }
+}
